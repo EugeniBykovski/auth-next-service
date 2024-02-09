3384e632a0fdb57c864046827c145ed64b054f30
Yauheni Bykouski <y.bykouski@godeltech.com>
2024-02-09 10:19:33 +0100

    updated some functionality

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,7 +1,22 @@
+"use client";
+
 import React from "react";
 import CardWrapper from "@/components/auth/card-wrapper";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginSchema } from "@/schemas";
+import { Form } from "../ui/form";
 
 const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      passwrd: "",
+    },
+  });
+
   return (
     <CardWrapper
       headerLabel="Welcome back"
@@ -9,7 +24,11 @@ const LoginForm = () => {
       showSocial
       backButtonHref="/auth/register"
     >
-      LoginForm
+      <Form {...form}>
+        <form className="space-y-6" onSubmit={form.handleSubmit(() => {})}>
+          <div></div>
+        </form>
+      </Form>
     </CardWrapper>
   );
 };
