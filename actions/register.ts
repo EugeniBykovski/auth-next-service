dbc2cbf9885e3fe7a65960de60bae092ebc070fd
Yauheni Bykouski <y.bykouski@godeltech.com>
2024-02-19 16:22:13 +0100

    implemented register and login functionality

--- actions/register.ts
+++ actions/register.ts
@@ -3,10 +3,28 @@
 import * as z from "zod";
 import bcrypt from "bcrypt";
 import { RegisterSchema } from "@/schemas";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validateFields = RegisterSchema.safeParse(values);
   if (!validateFields.success) return { error: "Invalid fields!" };
+
+  const { email, password, name } = validateFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+  if (existingUser) return { error: "Email already in use!" };
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: send verify token
 
   return { success: "Email was sent!" };
 };
